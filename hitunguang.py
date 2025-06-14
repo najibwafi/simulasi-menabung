6436839d45825d6b8b62561421fe310ff082ea65
najibwafi <wafinajib80@gmail.com>
2025-06-14 11:07:59 +0800

    update 14/6/2025

--- hitunguang.py
+++ hitunguang.py
@@ -2,13 +2,29 @@ import streamlit as st
 st.title("PROGRAM HITUNG TABUNGAN")
 i=1
 total=0
-tabungan=(st.number_input("masukan tabungan anda :"))
-lama_waktu=(st.number_input("masukan lama waktu :"))
+tabungan=(st.number_input("masukan tabungan anda :",
+                          min_value=1,
+                          value=1000000))
 
+lama_waktu=(st.number_input("masukan lama waktu :",
+                            min_value=1,
+                            value=1,
+                            step=1))
+bunga=st.slider("masukan bunga tahunan dalam persen",
+                min_value=0,
+                max_value=20,
+                value=2,
+                step=1)                            
+persen_bunga=bunga/(100*12)
 if st.button("hasil tabungan anda klik disini"):
-  while i<lama_waktu:
-    total=total+tabungan
-    total=total*1.02
-    i=i+1
-    st.write(f" tabungan anda pada bulanke {i} adalah : {total}")
-st.write(f"total tabungan anda pda bulan terakhir adalah :{total}")
+  if tabungan==0:
+    st.warning("uang tidak boleh 0")
+  elif lama_waktu==0:
+    st.warning("bulannya jangan 0")
+  else:
+    while i<lama_waktu:
+      total=total+tabungan
+      total=total*(1+persen_bunga)
+      i=i+1
+      st.success(f" tabungan anda pada bulanke {i} adalah :Rp. {int(total)}")
+st.success(f"total tabungan anda pda bulan terakhir adalah :Rp. {int(total)}")
